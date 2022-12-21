16e807be0136395c4172c3a3a3411b4eb3da07b8
Paul Garcia <pbrain19@gmail.com>
2022-12-21 16:40:19 -0500

    make a common lib

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -1,11 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { Configuration, CreateCompletionResponse, OpenAIApi } from "openai";
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+import { CreateCompletionResponse } from "openai";
+import { openai } from "./common";
 
 type RequestData = {
   title: string;
